b389184464b6491056c07fbd5ac19745ed82de85
Lisa <lisa-marie@kauck-online.de>
2022-09-21 09:27:22 +0200

    initalizing

--- src/Lorenz96andNDE.jl
+++ src/Lorenz96andNDE.jl
@@ -1,5 +1,21 @@
 module Lorenz96andNDE
 
-# Write your package code here.
+using DifferentialEquations
+function lorenz96!(dy,y,p,t) 
+    F, K = p
+    
+    dy[1]=(y[2]-y[K-1])*y[K]-y[1]+F;
+    dy[2]=(y[3]-y[K])*y[1]-y[2]+F;
+    dy[K]=(y[1]-y[K-2])*y[K-1]-y[K]+F;
+    for j=3:K-1
+        dy[j]=(y[j+1]-y[j-2])*y[j-1]-y[j]+F;
+    end
+end
+K = 36 # so 10 degrees of longtitude per node
+F = 0.5
+
+u0 = rand(K);
+prob = ODEProblem(lorenz96!, u0, (0.,100.), (F, K));
+sol = solve(prob);
 
 end
