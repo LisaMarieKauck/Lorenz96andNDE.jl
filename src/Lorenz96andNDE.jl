819d9fd23249902bc1d0192790a2232546037dd5
Lisa <lisa-marie@kauck-online.de>
2022-09-25 14:54:22 +0200

    adapting code to Lorenz96 model

--- src/Lorenz96andNDE.jl
+++ src/Lorenz96andNDE.jl
@@ -1,6 +1,8 @@
 module Lorenz96andNDE
 
-using DifferentialEquations, NODEData, Flux, DiffEqFlux 
+using DifferentialEquations, Flux, DiffEqFlux
+using  CUDA, OrdinaryDiffEq, BenchmarkTools, JLD2, Plots, Random
+#using NODEData, Flux, DiffEqFlux 
 function lorenz96!(dy,y,p,t) 
     F, K = p
     
@@ -11,12 +13,16 @@ function lorenz96!(dy,y,p,t)
         dy[j]=(y[j+1]-y[j-2])*y[j-1]-y[j]+F;
     end
 end
-K = 36 # so 10 degrees of longtitude per node
-F = 0.5
-dt=0.1
+K = 36; # so 10 degrees of longtitude per node
+F = 0.5;
+dt=0.1;
+N_t=500;
+t_transient=100.;
+tspan=(0., t_transient+N_t*0.1)
 u0 = rand(K);
-prob = ODEProblem(lorenz96!, u0, (0.,100.), (F, K));
-sol = solve(prob, Tsit5(), saveat=dt);
+
+prob = ODEProblem(lorenz96!, u0, tspan, (F, K));
+sol = solve(prob, Tsit5(), saveat=t_transient:dt:t_transient + N_t * dt);
 
 train, valid = NODEDataloader(sol, 10; dt=dt, valid_set=0.8)
 
@@ -31,17 +37,32 @@ predict(t, u0) = Array(solve(remake(node_prob; tspan=(t[1],t[end]),u0=u0, p=p), 
 
 loss(t, u0) = sum(abs2, predict(t, view(u0,:,1)) - u0)
 
+function plot_node()
+    plt = plot(valid.t, Array(predict(valid.t,valid.data[:,1])'), label="Neural ODE")
+    plot!(plt, valid.t, valid.data', label="Training Data",xlims=[125,150])
+    display(plt)
+end
+plot_node()
+
 opt = Flux.AdamW(1f-3)
 
-TRAIN = false
+Flux.train!(loss, Flux.params(p), train, opt)
+plot_node()
+
+λ_max = 0.9056 # maximum LE of the L63
+
+TRAIN = true
 if TRAIN 
     println("starting training...")
-    for i_e = 1:400
+    for i_e = 1:100
         Flux.train!(loss, Flux.params(p), train, opt)
+        #plot_node()
+        δ = ChaoticNDETools.forecast_δ(Array(predict(valid.t,valid.data[:,1])), valid.data)
+        forecast_length = findall(δ .> 0.4)[1][2] * dt * λ_max
+        println("forecast_length=", forecast_length)
+
         if (i_e % 30) == 0  # reduce the learning rate every 30 epochs
             opt[1].eta /= 2
         end
     end
-end
-
 end
